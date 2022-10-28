d88ba3fe27ce89bb9efea72f366d1a4ceb0cca32
Ulugbek-Akmalovich-Shodimurodov <ulugbekshodimurodov16@gmail.com>
2022-10-28 15:29:40 +0500

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,10 @@
+let navbarOpenBtn = document.querySelector('.site-navbar__bars')
+let navbarList = document.querySelector('.site__navbar-list')
+
+
+
+function navbarOpen(){
+    navbarList.classList.toggle('navbar__list-open')
+}
+
+navbarOpenBtn.addEventListener('click', navbarOpen);
