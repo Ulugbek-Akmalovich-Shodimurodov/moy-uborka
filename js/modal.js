e994e524354aa7430651f22c249208ff20ebe797
Ulugbek-Akmalovich-Shodimurodov <ulugbekshodimurodov16@gmail.com>
2022-11-01 09:19:52 +0500

    modal code finish

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+let cardListBtn = document.querySelector('.servicePage-list__item-link');
+let popup = document.querySelector('.popup');
+let openMContainer = document.querySelector('.modal-container');
+let modalBtn = document.querySelector('.modal-btn');
+let modalTitle = document.querySelector('.modal-title');
+let modalClose = document.querySelector('.modal-container')
+
+function openModal(){
+    popup.classList.add('open-popup');
+    openMContainer.classList.add('modal-container__event');
+}
+function closeModal(){
+    popup.classList.remove('open-popup');
+    openMContainer.classList.remove('modal-container-open');
+    modalClose.classList.remove('modal-container__event');
+}
+function openModalList(e){
+    modalTitle.textContent = `${e.target.textContent}`;
+    popup.classList.add('open-popup');
+    openModal();         
+    openMContainer.classList.add('modal-container-open');
+    console.log(e);
+}
+
+cardListBtn.addEventListener('click', openModalList);
+modalBtn.addEventListener('click', closeModal);
+modalClose.addEventListener('click', closeModal);
